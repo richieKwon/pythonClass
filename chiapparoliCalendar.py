f46e8ecd22e94e0ec6ae4c33bd0060692b1387f8
richie <richie.kwon1@gmail.com>
2023-03-18 14:59:49 +0000

    chiapparoli calendar

---
+++ chiapparoliCalendar.py
@@ -0,0 +1,22 @@
+from datetime import date, timedelta, datetime
+import holidays
+
+FYstartDate = datetime(2023, 4, 1)
+FYendDate = datetime(2024, 4, 1)
+it_holidays = holidays.country_holidays('IT')
+
+
+everyDaysList = [FYstartDate+timedelta(j)
+                 for j in range(0, (FYendDate - FYstartDate).days)]
+workingDaysList = [FYstartDate+timedelta(i) for i in range(0, (FYendDate - FYstartDate).days)
+                   if (FYstartDate+timedelta(i)).weekday() < 5]
+
+FYitalyHolidaysList = [everyDaysList[k] for k in range(
+    0, len(everyDaysList)) if everyDaysList[k] in it_holidays]
+
+FYshutdownPeriods = []
+chiapparoliCalendarList = [date.strftime('%Y%m%d') for date in list(
+    sorted(set(everyDaysList) - set(FYitalyHolidaysList))
+)]
+
+print(chiapparoliCalendarList)
